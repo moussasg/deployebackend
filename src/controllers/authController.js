bd1e5009dbd92017c2baf01b311f029592bda173
moussasg <117439847+moussasg@users.noreply.github.com>
2023-08-25 12:36:20 +0100

    back

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -132,11 +132,14 @@ const commands_post = async (req,res) => {
 }
 const signup_post = async (req, res) => {
   const { email, password } = req.body;
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  }
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(200).json({ success: true, user: user._id , token });
+    res.status(200).json({ success: true, user: user._id , token , headers});
   }
   catch (err) {
     const errors = handleErrors(err);
