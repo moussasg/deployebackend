5f9c00b808de92820399319d31ace953690eb08a
moussasg <117439847+moussasg@users.noreply.github.com>
2023-08-25 15:00:17 +0100

    ffff

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const {requireAuth} = require('./src/controllers/authController')
 const express = require('express')
 const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express()
 const mongoose = require('mongoose');
 const authController = require('./src/controllers/authController')
 const {checkUser } = require('./src/controllers/authController')
 dotenv.config();
+app.use(cors());
+// Set up CORS headers manually
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://soft-cannoli-96b536.netlify.app'); // Replace with your frontend domain
   res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
 app.use(express.json());
 app.post('/signup', authController.signup_post);
 app.post('/login', authController.login_post);
